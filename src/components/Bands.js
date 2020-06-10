78ad5d5a63a48d4461ecc02e8d477e491f039d1d
Leah Silber <LeahSilber@gmail.com>
2020-06-10 15:00:10 -0700

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -15,16 +15,4 @@ class Bands extends Component {
   }
 }
 
-// const Bands = (props) => {
-//   const bands = props.bands.map((band) => (
-//     <Band key={band.id} {...band} deleteBand={props.deleteBand} />
-//   ));
-
-//   return <div>{bands}</div>;
-// };
-
 export default Bands;
-
-// const bands = props.bands.map((band) => (
-//   <Band key={band.id} {...band} deleteBand={props.deleteBand} />
-// ));
